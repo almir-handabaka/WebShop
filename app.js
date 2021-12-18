7a8a69cfdc3e603ab32b6352c69e809569ba193c
Aleksej <virusgraffitimaker@gmail.com>
2021-12-19 00:13:46 +0100

    test commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var indexRouter = require('./routes/prijava');
 var authRouter = require('./routes/auth');
 
 var app = express();
-
+// komentar
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
