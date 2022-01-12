138e34b96af032e8cb905f86a779bfcd7cff046c
Aleksej <virusgraffitimaker@gmail.com>
2022-01-12 01:54:22 +0100

    update pocetna.ejs

--- routes/kupac/pocetna.js
+++ routes/kupac/pocetna.js
@@ -10,10 +10,25 @@ router.get('/', function (req, res, next) {
     }).catch((error) => {
         console.log(error);
         res.sendStatus(404);
-    })
+    });
 
 
 });
+
+router.get('/artikal/:naziv/:artikal_id', function (req, res, next) {
+    let artikal_id = req.params.artikal_id;
+
+    db_funkcije.dohvatiArtikal(artikal_id).then((result) => {
+        console.log(result);
+        res.render('kupac/artikal', { title: 'Web Shop', artikal: result });
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(404);
+    });
+
+
+});
+
 
 
 module.exports = router;
